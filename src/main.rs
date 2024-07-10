d4ea4f217f2b38acd7a02687cd77f3c8240999db
Lucemans <luc@lucemans.nl>
2024-07-10 02:19:44 +0200

    f

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use esp_idf_hal::{
-    delay::FreeRtos,
-    gpio::{IOPin, PinDriver},
-    peripherals::Peripherals,
+    delay::FreeRtos, gpio::{IOPin, PinDriver, Pull}, modem, peripherals::Peripherals, sys
 };
+use esp_idf_svc::{eventloop::EspSystemEventLoop, nvs::EspDefaultNvsPartition, wifi::{ClientConfiguration, Configuration}};
 
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
@@ -14,16 +13,38 @@ fn main() {
 
     log::info!("Hello, world!");
 
-    let peripherals = Peripherals::take().unwrap();
-    let mut led_pin = PinDriver::output(peripherals.pins.gpio6).unwrap();
+    // Important, do not remove, prevents device bricking
+    FreeRtos::delay_ms(5000);
 
-    led_pin.set_low().unwrap();
+    // Grab peripherals
+    let peripherals = Peripherals::take().unwrap();
+
+    let mut btn_pin = PinDriver::input(peripherals.pins.gpio6.downgrade()).unwrap();
+    btn_pin.set_pull(Pull::Down).unwrap();
 
     loop {
-        log::info!("Blinking LED");
-        led_pin.set_high().unwrap();
-        FreeRtos::delay_ms(1000);
-        led_pin.set_low().unwrap();
-        FreeRtos::delay_ms(1000);
+        // Loop through gpio pins and detect button presses
+        println!("Button pressed: {}", btn_pin.is_high());
+        FreeRtos::delay_ms(500);
+
+        // // Scan for wifi networks, list them over info!, and then wait 10 seconds loop
+        // let peripherals = Peripherals::take().unwrap();
+        // let modem = peripherals.modem;
+        // let sysloop = EspSystemEventLoop::take().unwrap();
+        // // let nvs = EspDefaultNvsPartition::take().unwrap();
+        // let mut wifi = esp_idf_svc::wifi::EspWifi::new(modem, sysloop.clone(), None).unwrap();
+
+        // wifi.set_configuration(&Configuration::Client(ClientConfiguration::default())).unwrap();
+        // wifi.start().unwrap();
+
+        // let scan = wifi.scan().unwrap();
+
+        // for network in scan.iter() {
+        //     log::info!("{:?}", network);
+        // }
+
+        // log::info!("Completed scan waiting 5 seconds");
+
+        // FreeRtos::delay_ms(5000);
     }
 }
